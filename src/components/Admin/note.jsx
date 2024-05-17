85d52c78888a6ec7e48842364227bace5bc7111c
Omole Ayodeji <omoledaniel13@gmail.com>
2024-05-17 13:03:48 +0100

    Update note.jsx

--- src/components/Admin/note.jsx
+++ src/components/Admin/note.jsx
@@ -1,28 +1,28 @@
-'use client';
-import { ISidebarLink, links } from '@/static/index';
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useState } from 'react';
+// 'use client';
+// import { ISidebarLink, links } from '@/static/index';
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { usePathname } from 'next/navigation';
+// import { useState } from 'react';
 
-import { FaHeadphonesAlt } from 'react-icons/fa';
-import { FiSettings } from 'react-icons/fi';
-import { CgMenu } from 'react-icons/cg';
-import { CgClose } from 'react-icons/cg';
-import logoIcon from '../../../public/logo-icon.png';
-import logo from '../../../public/logo.png';
-import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
+// import { FaHeadphonesAlt } from 'react-icons/fa';
+// import { FiSettings } from 'react-icons/fi';
+// import { CgMenu } from 'react-icons/cg';
+// import { CgClose } from 'react-icons/cg';
+// import logoIcon from '../../../public/logo-icon.png';
+// import logo from '../../../public/logo.png';
+// import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
 
-type SidebarProps = {
-  isOpen: boolean;
-  toggleSidebar: () => void;
-};
+// type SidebarProps = {
+//   isOpen: boolean;
+//   toggleSidebar: () => void;
+// };
 
-export default function AdminSidebar({ isOpen, toggleSidebar }: SidebarProps) {
-  const pathname = usePathname();
-  const [isExpanded, setIsExpanded] = useState<null | number>(null);
+// export default function AdminSidebar({ isOpen, toggleSidebar }: SidebarProps) {
+//   const pathname = usePathname();
+//   const [isExpanded, setIsExpanded] = useState<null | number>(null);
 
-  return (
+//   return (
     
-  );
-}
+//   );
+// }
