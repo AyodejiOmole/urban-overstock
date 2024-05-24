a7a415bdeed7d122f1b89401b7f15ec4360b71ce
AyodejiOmole <omoledaniel13@gmail.com>
2024-05-24 04:39:13 +0100

    removed product tag for update product too

--- src/components/Admin/Products/ProductForm.tsx
+++ src/components/Admin/Products/ProductForm.tsx
@@ -240,7 +240,7 @@ export default function ProductForm({
     initialValues: {
       name: activeProduct?.name ?? " ",
       description: activeProduct?.description ?? " ",
-      tag: activeProduct?.tag ?? " ",
+      // tag: activeProduct?.tag ?? " ",
       brandId: activeProduct?.brandId ?? undefined,
       quantity: activeProduct?.quantity ?? undefined,
       amount: activeProduct?.amount ?? undefined,
@@ -261,7 +261,7 @@ export default function ProductForm({
     validationSchema: Yup.object({
       name: Yup.string().required().label('Name'),
       description: Yup.string().required().label('Description'),
-      tag: Yup.string().required().label('Tag'),
+      // tag: Yup.string().required().label('Tag'),
       quantity: Yup.number().min(1).required().label('Quantity'),
       // amount: Yup.number().min(1).required().label('Price'),
       amount: Yup.number().min(1).required().label('Price').test('amount', 'Amount cannot exceed be less than cost price.', function () {
@@ -336,6 +336,7 @@ export default function ProductForm({
               categoryId: values.categoryId ?? 0,
               productVarations: getFormattedVariations(state),
               imageUrls: product_images,
+              tag: "empty",
             };
 
             console.log('Request Body: ', data);
@@ -1252,7 +1253,7 @@ export default function ProductForm({
             <CustomError error={formik.errors.categoryId} />
           </div>
           {/* Product Tags */}
-          <div className='mb-6'>
+          {/* <div className='mb-6'>
             <label htmlFor='tag' className='text-sm text-neutral mb-2 block'>
               Product Tag
             </label>
@@ -1263,7 +1264,7 @@ export default function ProductForm({
               value={formik.values.tag}
               error={formik.errors.tag}
             />
-          </div>
+          </div> */}
         </div>
 
         {/* Product Status */}
