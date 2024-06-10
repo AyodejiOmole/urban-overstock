940ebb558f5d0b8bdd720a03a14fdc9bc649f3b0
Omole Ayodeji <omoledaniel13@gmail.com>
2024-06-11 00:07:39 +0100

    Update CancelledOrderDetails.tsx: removed duplicate "use client" declaration.

--- src/components/Admin/Orders/CancelledOrderDetails.tsx
+++ src/components/Admin/Orders/CancelledOrderDetails.tsx
@@ -1,5 +1,4 @@
 "use client";
-"use client";
 import React from 'react';
 import { CiLocationOn, CiLock, CiMail, CiShoppingCart } from 'react-icons/ci';
 import { MdOutlineAlternateEmail } from 'react-icons/md';
